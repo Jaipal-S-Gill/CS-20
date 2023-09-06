d731f8d2203f83124f8dda07cd6cf0b4a824376c
Jaipal-S-Gill <124804130+Jaipal-S-Gill@users.noreply.github.com>
2023-09-06 13:28:52 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+// Olympic Rings Start Code
+
+// Canvas Setup
+let cnv = document.getElementById("my-canvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 440;
+cnv.height = 250;
+
+// Call draw function once all page resources have loaded
+window.addEventListener("load", draw);
+
+function draw() {
+  // Background
+  ctx.fillStyle = "#eee";
+  ctx.fillRect(0, 0, cnv.width, cnv.height);
+
+  // Circle Line Width
+  ctx.lineWidth = 8;
+  strokeCircle(100, 100, 50, "blue");
+  strokeCircle(160, 150, 50, "yellow");
+  strokeCircle(220, 100, 50, "black");
+  strokeCircle(280, 150, 50, "green");
+  strokeCircle(340, 100, 50, "red");
+  strokeCircle(0, 0, 100, "purple");
+}
+
+// Draw a stroke circle at center (x,y) with a radius of r
+function strokeCircle(x, y, r, color) {
+  ctx.strokeStyle = color;
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.stroke();
+}
